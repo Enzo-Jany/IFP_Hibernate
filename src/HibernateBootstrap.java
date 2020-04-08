49eb8278bee14175df2996363fd9363c3dc9fd45
Enzo-Jany <enzo.jany@campus-igs-toulouse.fr>
2020-04-08 10:54:25 +0200

    initial commit + librairie hibernate ajouté + persistance créer

---
+++ src/HibernateBootstrap.java
@@ -0,0 +1,8 @@
+import javax.persistence.Persistence;
+
+public class HibernateBootstrap {
+
+    protected void setUp() throws Exception {
+    //sessionFactory = Persistence.createEntityManagerFactory( "org.hibernate.tutorial.jpa" );
+}
+}
